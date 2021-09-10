cfcc7c5af9b8b9216a467e9ba765442adb832276
Scott Stephens <stephens.js@gmail.com>
2021-09-10 18:41:03 -0500

    First attempt at getting npm and webpack working

---
+++ src/content/StarterWeb-CSharp/Client/js/site.js
@@ -0,0 +1,6 @@
+﻿import 'bootstrap/dist/css/bootstrap.css';
+
+// Custom CSS imports
+import '../css/site.css';
+
+// Write your JavaScript code.
---
+++ src/content/StarterWeb-CSharp/Client/webpack.config.js
@@ -0,0 +1,30 @@
+﻿const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: {
+        site: './src/js/site.js',
+        bootstrap_js: './src/js/bootstrap_js.js',
+        validation: './src/js/validation.js'
+    },
+    output: {
+        filename: '[name].entry.js',
+        path: path.resolve(__dirname, '..', 'wwwroot', 'dist')
+    },
+    devtool: 'source-map',
+    mode: 'development',
+    module: {
+        rules: [
+            { test: /\.css$/, use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader"] },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
+            { test: /\.(woff|woff2)$/, loader: "url-loader", options: { prefix: "font", limit: 5000 } },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader", options: { limit: 10000, mimetype: "application/octet-stream" } },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader", options: { limit: 10000, mimetype: "image/svg+xml" } }
+        ]
+    },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "[name].css"
+            })
+        ]
+};
